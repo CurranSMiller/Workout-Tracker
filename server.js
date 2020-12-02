1f6cf6ea502c2942b88c2679b3f8bcacb16bbcb1
Curran Miller <curransmiller@gmail.com>
2020-12-02 17:27:48 -0500

    deploying

--- server.js
+++ server.js
@@ -23,4 +23,4 @@ app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port ${PORT}!`)})
