471420770001e062b6bf1e7cb72ffca698d80aa6
Curran Miller <curransmiller@gmail.com>
2020-12-02 17:20:30 -0500

    deploying

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
-  
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 const app = express();
 
 app.use(logger("dev"));
 
-app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-require('./routes')(app);
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
+app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
   useFindAndModify: false
 });
 
@@ -29,4 +24,3 @@ app.use(require("./routes/view.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
-});
